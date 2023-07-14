6b17d36683272370bf9e24ee7bf4a9af78e820ed
Wellington Luiz <42124380+wlusbar@users.noreply.github.com>
2023-07-14 16:15:26 -0300

    Style pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {FiSearch} from 'react-icons/fi';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='container'>
+          <h1 className='title'>Buscar Logradouro</h1>
+
+          <div className='containerInput'>
+            <input 
+            type="text" 
+            placeholder='Digite o CEP'            
+            />
+
+            <button className='buttonSeach'>
+              <FiSearch size={25} color="#FFF" />
+            </button>
+          </div>
+
+          <main className='main'>
+            <h2>CEP: 00000</h2>
+
+            <span>nome da rua</span>
+            <span>Complementos</span>
+            <span>Bairro</span>
+            <span>Cidade</span>
+          </main>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
